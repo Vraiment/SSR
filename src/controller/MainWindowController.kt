559399c11a60db8b62a8eb776fa6aa79d5d053b6
Vraiment <jemc44@gmail.com>
2018-12-29 23:35:43 -0800

    Added logic to show the file chooser

--- src/controller/MainWindowController.kt
+++ src/controller/MainWindowController.kt
@@ -3,7 +3,9 @@ package vraiment.sage.savereader.controller
 import javafx.fxml.FXML
 import javafx.scene.control.Label
 import javafx.scene.layout.VBox
+import javafx.stage.FileChooser
 import javafx.stage.Stage
+import java.io.File
 
 class MainWindowController {
     @FXML
@@ -16,11 +18,22 @@ class MainWindowController {
         root.scene.window as Stage
     }
 
+    private companion object {
+        val FILE_CHOOSER = FileChooser().also {
+            it.title = "Open save file..."
+            it.extensionFilters.add(FileChooser.ExtensionFilter("Sage save file", "*sav"))
+        }
+    }
+
     fun displayOpenFileWindow() {
-        TODO()
+        FILE_CHOOSER.showOpenDialog(stage)?.also { loadFile(it) }
     }
 
     fun quit() {
         stage.close()
+    }
+
+    private fun loadFile(file: File) {
+        TODO()
     }
 }
