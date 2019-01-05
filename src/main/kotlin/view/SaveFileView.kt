13ddc69cbe1724ac9d6f6dd36129e72b23c653f6
Vraiment <jemc44@gmail.com>
2019-01-04 22:49:31 -0800

    Updated the DSL to be simpler and use Kotlin's support for DSLs.

--- src/main/kotlin/view/SaveFileView.kt
+++ src/main/kotlin/view/SaveFileView.kt
@@ -8,77 +8,80 @@ import vraiment.sage.ssr.model.Time
 import vraiment.sage.ssr.view.dsl.treeItem
 import java.nio.ByteBuffer
 
-fun SaveFile.getView(saveFileName: String) = treeItem(saveFileName) {
-    children {
-        treeItem("version = ${version.displayString}") // Byte
-        treeItem("level = $level") // Byte
-        treeItem("area = \"${area.displayString}\"") // Id
-        treeItem("position = \"${position.displayString}\"") // Id
-        treeItem("look = \"${look.displayString}\"") // Id
-        treeItem("toa = \"${toa.displayString}\"") // Id
-        treeItem("mask = $mask") // Int
-        treeItem("glyph = $glyph") // Byte
-        treeItem("health = $health") // Byte
-        treeItem("energy = $energy") // Byte
-        treeItem("tokens (${tokens.size})") { // List<IdInArea>
-            children {
-                tokens.map { treeItem(it.displayString) }
-            }
-        }
-        treeItem("maskList (${maskList.size})") { // List<IdInArea>
-            children {
-                maskList.map { treeItem(it.displayString) }
-            }
-        }
-        treeItem("glyphs (${glyphs.size})") { // List<IdInArea>
-            children {
-                glyphs.map { treeItem(it.displayString) }
-            }
-        }
-        treeItem("inventory (${inventory.size})") { // List<IdInArea>
-            children {
-                inventory.map { treeItem(it.displayString) }
-            }
-        }
-        treeItem("cinema (${cinema.size})") { // List<Id>
-            children {
-                cinema.map { treeItem(it.displayString) }
-            }
-        }
-        treeItem("conversations") { // Conversations
-            children {
-                treeItem("conditions = ${conversations.conditions}")
-                treeItem("entries (${conversations.entries.size})") {
-                    children {
-                        conversations.entries.map { treeItem(it.displayString) }
-                    }
-                }
-            }
-        }
-        treeItem("times (${times.size})") { // List<Time>
-            children {
-                times.map { treeItem(it.displayString) }
-            }
-        }
-        treeItem("quests") { // Quests
-            children {
-                treeItem("quest 0 = ${quests.quest0}")
-                treeItem("quest 1 = ${quests.quest1}")
-                treeItem("quest 2 = ${quests.quest2}")
-                treeItem("quest 3 = ${quests.quest3}")
-                treeItem("quest 4 = ${quests.quest4}")
-                treeItem("quest 5 = ${quests.quest5}")
+fun SaveFile.getView(saveFileName: String) = treeItem<String> {
+    value = saveFileName
 
-            }
+    treeItem { value = "version = ${version.displayString}" } // Byte
+    treeItem { value = "level = $level" } // Byte
+    treeItem { value = "area = \"${area.displayString}\"" } // Id
+    treeItem { value = "position = \"${position.displayString}\"" } // Id
+    treeItem { value = "look = \"${look.displayString}\"" } // Id
+    treeItem { value = "toa = \"${toa.displayString}\"" } // Id
+    treeItem { value = "mask = $mask" } // Int
+    treeItem { value = "glyph = $glyph" } // Byte
+    treeItem { value = "health = $health" } // Byte
+    treeItem { value = "energy = $energy" } // Byte
+    treeItem {
+        value = "tokens (${tokens.size})" // List<IdInArea>
+        tokens.map {
+            treeItem { value = it.displayString }
         }
-        treeItem("masks = $masks") // Byte?
-        treeItem("glyphFound = $glyphFound") // Byte?
-        treeItem("stoneFound = $stoneFound") // Byte?
-        treeItem("hookFound = $hookFound") // Byte?
-        treeItem("ammo = $ammo") // Short?
     }
+    treeItem {
+        value = "maskList (${maskList.size})" // List<IdInArea>
+        maskList.map {
+            treeItem { value = it.displayString }
+        }
+    }
+    treeItem {
+        value = "glyphs (${glyphs.size})" // List<IdInArea>
+        glyphs.map {
+            treeItem { value = it.displayString }
+        }
+    }
+    treeItem {
+        value = "inventory (${inventory.size})" // List<IdInArea>
+        inventory.map {
+            treeItem { value = it.displayString }
+        }
+    }
+    treeItem {
+        value = "cinema (${cinema.size})" // List<Id>
+        cinema.map {
+            treeItem { value = it.displayString }
+        }
+    }
+    treeItem {
+        value = "conversations" // Conversations
+        treeItem { value = "conditions = ${conversations.conditions}" }
+        treeItem {
+            value = "entries (${conversations.entries.size})"
+            conversations.entries.map {
+                treeItem { value = it.displayString }
+            }
+        }
+    }
+    treeItem {
+        value = "times (${times.size})" // List<Time>
+        times.map {
+            treeItem { value = it.displayString }
+        }
+    }
+    treeItem {
+        value = "quests" // Quests
+        treeItem { value = "quest 0 = ${quests.quest0}" }
+        treeItem { value = "quest 1 = ${quests.quest1}" }
+        treeItem { value = "quest 2 = ${quests.quest2}" }
+        treeItem { value = "quest 3 = ${quests.quest3}" }
+        treeItem { value = "quest 4 = ${quests.quest4}" }
+        treeItem { value = "quest 5 = ${quests.quest5}" }
+    }
+    treeItem { value = "masks = $masks" } // Byte?
+    treeItem { value = "glyphFound = $glyphFound" } // Byte?
+    treeItem { value = "stoneFound = $stoneFound" } // Byte?
+    treeItem { value = "hookFound = $hookFound" } // Byte?
+    treeItem { value = "ammo = $ammo" } // Short?
 }
-
 
 private fun Int.toByteArray() = ByteBuffer.allocate(4).putInt(this).array()
 
--- src/main/kotlin/view/dsl/TreeItem.kt
+++ src/main/kotlin/view/dsl/TreeItem.kt
@@ -3,87 +3,35 @@ package vraiment.sage.ssr.view.dsl
 import javafx.scene.control.TreeItem
 
 /*
- This file contains a small "DSL" (Domain Specific Language) to create TreeItems for a TreeView:
-
- - Use the function `treeItem` to create a new TreeItem.
- - To add children to a TreeItem use the member children: `treeItem(value) { children { ... } } `
+ This file contains a small "DSL" (Domain Specific Language) for JavaFX.
  */
 
 /**
- * Creates a new TreeItem with the input value, and the configuration from the input block.
+ * Annotation to mark functions that as part of the DSL.
+ */
+@DslMarker
+annotation class JavaFx
+
+/**
+ * Creates a new TreeItem and uses the given block to configure it.
  *
- * @param value The value for the TreeItem
- * @param block The block to configure the TreeItem
+ * @param block The block to configure the TreeItem.
  *
- * @return A TreeItem with the input value and configured according to the input block.
+ * @return A TreeItem with configured according to the input block.
  */
-fun <T> treeItem(value: T, block: TreeItemBuilder<T>.() -> Unit) = TreeItemBuilder<T>()
-        .also { it.value = value }
-        .apply(block)
-        .build()
+@JavaFx
+fun <T> treeItem(block: TreeItem<T>.() -> Unit): TreeItem<T> = TreeItem<T>()
+    .apply(block)
+
 
 /**
- * Class to build a and configure a TreeItem object.
+ * Adds a new tree item to the given tree and uses the given block to configure it.
+ *
+ * @param block The block to configure the TreeItem.
+ *
+ * @return A TreeItem (children of the given TreeItem) configured according to the input block.
  */
-class TreeItemBuilder<T> {
-    /**
-     * The value of the tree item object.
-     */
-    var value: T? = null
-
-    /**
-     * Adds any TreeItem provided by the input block to the TreeItem that will be build.
-     *
-     * @param block The block to configure
-     *
-     * @return The same instance of TreeItemBuilder<T>.
-     */
-    fun children(block: TreeItems<T>.() -> Unit) = TreeItems<T>()
-            .apply(block)
-            .also { children.addAll(it) }
-            .let { this }
-
-    /**
-     * Builds a TreeItem instance with the values from the builder.
-     *
-     * @return A TreeItem<T> with the values from the builder.
-     */
-    fun build(): TreeItem<T> {
-        val result = TreeItem<T>()
-
-        result.value = value
-        result.children.addAll(children)
-
-        return result
-    }
-
-    private val children = mutableListOf<TreeItem<T>>()
-}
-
-/**
- * Helper class to allow create a list of TreeItems<T>.
- */
-class TreeItems<T> : ArrayList<TreeItem<T>>() {
-    /**
-     * Adds tree item with the given input value to the list of tree items.
-     *
-     * @param value The value for the TreeItem.
-     *
-     * @return The created TreeItem.
-     */
-    fun treeItem(value: T) = TreeItem(value).also { add(it) }
-
-    /**
-     * Adds a tree item with the given input value to the list of tree items and configures it using the input block.
-     *
-     * @param value The value for the TreeItem.
-     * @param block The block to configure the TreeItem.
-     *
-     * @return The created TreeItem.
-     */
-    fun treeItem(value: T, block: TreeItemBuilder<T>.() -> Unit) = TreeItemBuilder<T>()
-            .also { it.value = value }
-            .apply(block)
-            .build()
-            .also { add(it) }
-}
+@JavaFx
+fun <T> TreeItem<T>.treeItem(block: TreeItem<T>.() -> Unit = {}): TreeItem<T> = TreeItem<T>()
+    .apply(block)
+    .also { children.add(it) }
